da74491c93868c4cd8ef19fcaeb2fd8a58957a4d
manhbd-22022602 <22022602@vnu.edu.vn>
2024-08-31 23:23:47 +0700

    Fix bug

--- run_inference.py
+++ run_inference.py
@@ -1,8 +1,6 @@
 import torch
-
 import cv2
 import numpy as np
-import pandas as pd
 from path import Path
 import argparse
 from tqdm import tqdm
@@ -22,11 +20,8 @@ parser.add_argument("--no-resize", action='store_true', help="no resizing is don
 
 parser.add_argument("--dataset-list", default=None, type=str, help="Dataset list file")
 parser.add_argument("--dataset-dir", default='.', type=str, help="Dataset directory")
-parser.add_argument("--image-list", default=None, type=str, help="File with list of image paths")
 parser.add_argument("--output-dir", default='output', type=str, help="Output directory")
 parser.add_argument("--img-exts", default=['png', 'jpg', 'bmp'], nargs='*', type=str, help="images extensions to glob")
-parser.add_argument("--position", required=True, type=str, help="Position identifier for output files")
-parser.add_argument("--batch-size", default=1, type=int, help="Batch size for inference")
 parser.add_argument('--resnet-layers', required=True, type=int, default=18, choices=[18, 50],
                     help='depth network architecture.')
 
@@ -36,59 +31,52 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 @torch.no_grad()
 def main():
     args = parser.parse_args()
-    if not (args.output_disp or args.output_depth):
-        print('You must at least output one value!')
+    if not(args.output_disp or args.output_depth):
+        print('You must at least output one value !')
         return
 
     disp_net = DispResNet(args.resnet_layers, False).to(device)
-    weights = torch.load(args.pretrained, map_location=device)
+    weights = torch.load(args.pretrained)
     disp_net.load_state_dict(weights['state_dict'])
     disp_net.eval()
 
-    output_dir = Path(args.output_dir) / args.position
+    dataset_dir = Path(args.dataset_dir)
+    output_dir = Path(args.output_dir)
     output_dir.makedirs_p()
 
-    # Read image paths from the file
-    image_paths_df = pd.read_csv(args.image_list, header=None, sep='\t')
-    image_paths = image_paths_df[0].tolist()  # Assuming image paths are in the first column
+    if args.dataset_list is not None:
+        with open(args.dataset_list, 'r') as f:
+            test_files = [dataset_dir/file for file in f.read().splitlines()]
+    else:
+        test_files = sum([dataset_dir.files('*.{}'.format(ext)) for ext in args.img_exts], [])
 
-    print('{} files to test'.format(len(image_paths)))
+    print('{} files to test'.format(len(test_files)))
 
-    # Create batches from image paths
-    for i in tqdm(range(0, len(image_paths), args.batch_size)):
-        batch_files = image_paths[i:i + args.batch_size]
+    for file in tqdm(test_files):
 
-        # Load and preprocess images
-        images = []
-        for file in batch_files:
-            img = cv2.imread(file)
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            h, w, _ = img.shape
-            if (not args.no_resize) and (h != args.img_height or w != args.img_width):
-                img = cv2.resize(img, (args.img_width, args.img_height), interpolation=cv2.INTER_LINEAR).astype(np.float32)
-            img = np.transpose(img, (2, 0, 1))
+        img = cv2.imread(str(file))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-            images.append(img)
+        h, w, _ = img.shape
+        if (not args.no_resize) and (h != args.img_height or w != args.img_width):
+            img = cv2.resize(img, (args.img_width, args.img_height), interpolation=cv2.INTER_LINEAR)
+        img = np.transpose(img, (2, 0, 1))
 
-        tensor_imgs = torch.from_numpy(np.stack(images)).to(device)
-        tensor_imgs = ((tensor_imgs / 255 - 0.45) / 0.225)
-        print(tensor_imgs.shape)
-        # Predict with batch
-        outputs = disp_net(tensor_imgs)
+        tensor_img = torch.from_numpy(img).float().unsqueeze(0)
+        tensor_img = ((tensor_img/255 - 0.45)/0.225).to(device)
 
-        # Save results
-        for j, file in enumerate(batch_files):
-            output = outputs[j]
-            file_path, file_ext = Path(file).stem, Path(file).suffix
-            file_name = file_path.replace('/', '_')
+        output = disp_net(tensor_img)[0]
 
-            if args.output_disp:
-                disp = (255 * tensor2array(output, max_value=None, colormap='bone')).astype(np.uint8)
-                cv2.imwrite(str(output_dir / '{}_disp{}'.format(file_name, file_ext)), cv2.cvtColor(np.transpose(disp, (1, 2, 0)), cv2.COLOR_RGB2BGR))
-            if args.output_depth:
-                depth = 1 / output
-                depth = (255 * tensor2array(depth, max_value=10, colormap='rainbow')).astype(np.uint8)
-                cv2.imwrite(str(output_dir / '{}_depth{}'.format(file_name, file_ext)), cv2.cvtColor(np.transpose(depth, (1, 2, 0)), cv2.COLOR_RGB2BGR))
+        file_path, file_ext = file.relpath(args.dataset_dir).splitext()
+        file_name = '-'.join(file_path.splitall())
+
+        if args.output_disp:
+            disp = (255*tensor2array(output, max_value=None, colormap='bone')).astype(np.uint8)
+            cv2.imwrite(str(output_dir/'{}_disp{}'.format(file_name, file_ext)), cv2.cvtColor(np.transpose(disp, (1, 2, 0)), cv2.COLOR_RGB2BGR))
+        if args.output_depth:
+            depth = 1/output
+            depth = (255*tensor2array(depth, max_value=10, colormap='rainbow')).astype(np.uint8)
+            cv2.imwrite(str(output_dir/'{}_depth{}'.format(file_name, file_ext)), cv2.cvtColor(np.transpose(depth, (1, 2, 0)), cv2.COLOR_RGB2BGR))
 
 
 if __name__ == '__main__':
